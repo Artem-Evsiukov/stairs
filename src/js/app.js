6d437d627025234e965b65fb3658fd5fc03b6da3
maluy <artem.evsiukov1@gmail.com>
2022-04-30 11:17:56 +0300

    add

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,12 @@
 //
 // This file will be compiled into app.js and will not be minified.
 // Feel free with using ES6 here.
-
-//import Accordion from './components/accordion';
+import burgerMenu from './components/burger-menu';
 
 (($) => {
   // When DOM is ready
   $(() => {
     //const accordions = new Accordion();
-    //burgerMenu.init();
+    burgerMenu.init();
   });
 })(jQuery);
