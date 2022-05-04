4578a11a7e618b1ee193c8269db2a846f80256d0
maluy <artem.evsiukov1@gmail.com>
2022-05-04 16:39:38 +0300

    add

--- src/js/app.js
+++ src/js/app.js
@@ -20,6 +20,7 @@ import { inputmask } from './components/inputMask';
     inputmask();
   });
 })(jQuery);
+
 var scrolled;
 window.onscroll = function () {
   scrolled = window.pageYOffset || document.documentElement.scrollTop;
